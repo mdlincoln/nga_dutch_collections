b83c69ddd0f1763d810d995cba58a41cdb60e1e9
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-29 11:00:01 -0400

    outlines for scraping scripts

---
+++ scrape/scraper.rb
@@ -0,0 +1,17 @@
+require "open-uri"
+require "nokogiri"
+
+# Retrieve main collection page with list
+def object_url_list(baseurl)
+  object_url_list = []
+
+  end
+  # Get all object works elements and return them as an array of urls
+
+  return object_url_list
+end
+
+# Download all html to a file for later parsing
+def download_content(url)
+
+end
