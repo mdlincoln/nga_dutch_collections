c78ae6e7402f2518f04d60460c84d1311caf2350
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-29 11:09:03 -0400

    Retrive each link

--- scrape/scraper.rb
+++ scrape/scraper.rb
@@ -4,9 +4,13 @@ require "nokogiri"
 # Retrieve main collection page with list
 def object_url_list(baseurl)
   object_url_list = []
+  raw_html = open(baseurl, :read_timeout => 60)
+  parsed_html = Nokogiri::HTML(raw_html)
 
-  end
   # Get all object works elements and return them as an array of urls
+  parsed_html.css(".oe-drawer-list li").each do |link|
+    object_url_list << link.at("a @href")
+  end
 
   return object_url_list
 end
