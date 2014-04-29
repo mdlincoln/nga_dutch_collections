8119771a41a302bad3fbe52e17a240e6ee4167af
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-29 11:45:37 -0400

    Framework for parsing paintings

--- scrape/NGAPainting.rb
+++ scrape/NGAPainting.rb
@@ -1,5 +1,23 @@
 require "nokogiri"
+require "mongo"
 
-Class NGAPainting
+DATABASE = Mongo::MongoClient.new["arth"]["nga_dutch"]
 
+def import_objects(filelist)
+  filelist.each do |path|
+    object_hash = parse_file(path)
+    DATABASE.insert(object_hash)
+end
+
+def parse_file(path)
+  # Load and parse HTML
+  raw_html = File.read(path)
+  parsed_html = Nokogiri::HTML(raw_html)
+
+  object_data = {}
+
+  # Store key fields from HTML
+
+  # Return object_data hash
+  return object_data
 end
