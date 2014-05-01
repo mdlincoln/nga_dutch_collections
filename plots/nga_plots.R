0a4e74583faee08c33fd7df4a88dd951b7766e68
mdlincoln <matthew.d.lincoln@gmail.com>
2014-05-01 00:22:45 -0400

    basic plots

--- plots/nga_plots.R
+++ plots/nga_plots.R
@@ -18,3 +18,7 @@ collection_data$creation_date <- as.numeric(str_match(collection_data$created, d
 ##### exctract accession year #####
 acc_regex <- "([0-9]{4})[.]"
 collection_data$acc_date <- as.numeric(str_match(collection_data$accession, acc_regex)[,2])
+
+ggplot(collection_data, aes(x=acc_date, y=creation_date, size=height, color=medium)) + geom_point(shape=1) + scale_area(range=c(5,30))
+
+ggplot(filter(collection_data, medium %in% c("oil on canvas", "oil on panel")), aes(x=acc_date, y=height*width, color=medium)) + geom_point(shape=1) + geom_smooth()
