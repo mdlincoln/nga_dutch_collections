02c408f1bf22c19b289e3a55653806d0b86a3a06
mdlincoln <matthew.d.lincoln@gmail.com>
2014-05-01 00:22:35 -0400

    Extract dimensions, creation, and acquisition date

--- plots/nga_plots.R
+++ plots/nga_plots.R
@@ -1,1 +1,20 @@
 library("ggplot2")
+library("stringr")
+library("dplyr")
+
+collection_data <- read.csv("../collection_data.csv", stringsAsFactors=FALSE)
+collection_data$medium <- as.factor(collection_data$medium)
+collection_data$artist <- as.factor(collection_data$artist)
+
+##### Extract hight and width measurements #####
+dim_regex <- "([0-9]{1,}[.]?[0-9]?) x ([0-9]{1,}[.]?[0-9]?)"
+collection_data$width <- as.numeric(str_match(collection_data$dimensions, dim_regex)[,2])
+collection_data$height <- as.numeric(str_match(collection_data$dimensions, dim_regex)[,3])
+
+##### extract year created #####
+date_regex <- "([0-9]{4})"
+collection_data$creation_date <- as.numeric(str_match(collection_data$created, date_regex)[,2])
+
+##### exctract accession year #####
+acc_regex <- "([0-9]{4})[.]"
+collection_data$acc_date <- as.numeric(str_match(collection_data$accession, acc_regex)[,2])
